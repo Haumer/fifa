46de44bc3d5733fc9d397d63401b9112487e59eb
Alexander Haumer <alexander.haumer@me.com>
2018-06-09 13:42:03 +0100

    match model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20180609122908) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "matches", force: :cascade do |t|
+    t.integer "match_id"
+    t.string "home_country"
+    t.string "away_country"
+    t.string "date"
+    t.string "location"
+    t.string "group"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
