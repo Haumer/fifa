7ff0977643f6100c7ba1b23fd4f4b5d45f3a9c5a
Alexander Haumer <alexander.haumer@me.com>
2018-06-12 02:31:59 +0100

    navbar + footer

---
+++ app/javascript/components/navbar.js
@@ -0,0 +1,23 @@
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('date_time_now').innerHTML =
+    h + ":" + m + ":" + s;
+    var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};
+    return i;
+}
+
+function navbarChangeColor() {
+  const navbar = document.querySelector("")
+  window.addEventListener("scroll", (e) =>
+  console.log(e));
+}
+
+export { navbarChangeColor };
