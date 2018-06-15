8967a29ab941e773cf83d1f6c78f781c207dd27c
Alexander Haumer <alexander.haumer@me.com>
2018-06-16 00:04:14 +0100

    job working again | Morroco -> Morocco

--- app/jobs/results_job.rb
+++ app/jobs/results_job.rb
@@ -1,7 +1,10 @@
+require "open-uri"
+require "json"
+
 class ResultsJob < ApplicationJob
   queue_as :default
 
-  def perform(*group_id)
+  def perform(group_id)
     root = "http://livescore-api.com/api-client"
     groupstage = "#{ENV['RESULTS_URL']}"
     response = open(root + groupstage + group_id.to_s)
@@ -11,13 +14,20 @@ class ResultsJob < ApplicationJob
     teams = Team.all
     teams_group = teams.where(group_id: group_id)
     puts "1"
+    teams_group.each { |e| p e.name }
+    puts "2"
     json["data"]["table"].each do |data|
-      if data["name"] == "Morroco"
-        placeholder = Morocco
+      if data["name"] == "Morocco"
+        placeholder = "Morroco"
+        p placeholder
       else
         placeholder = data["name"]
+        p placeholder
       end
+      p "#{placeholder} is here"
+
       team = teams_group.where(name: placeholder)
+      p team.first
       team.first.wins = data["won"]
       team.first.losses = data["lost"]
       team.first.draws = data["drawn"]
--- app/jobs/update_live_score_job.rb
+++ app/jobs/update_live_score_job.rb
@@ -5,6 +5,24 @@ class UpdateLiveScoreJob < ApplicationJob
   queue_as :default
 
   def perform(*group_id)
+    root = "http://livescore-api.com/api-client"
+    groupstage = "#{ENV['RESULTS_URL']}"
+    response = open(root + groupstage + group_id.to_s)
+    response_body = response.read
+    json = JSON.parse(response_body)
 
+    teams = Team.all
+    teams_group = teams.where(group_id: group_id)
+
+    json["data"]["table"].each do |data|
+      team = teams_group.where(name: data["name"])
+      team.first.wins = data["won"]
+      team.first.losses = data["lost"]
+      team.first.draws = data["drawn"]
+      team.first.points = data["points"]
+      team.first.goals_for = data["goals_scored"]
+      team.first.goals_against = data["goals_conceded"]
+      team.first.save
+    end
   end
 end
