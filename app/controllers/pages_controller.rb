0ea4e68880f83697ab7fff37c4fa51e3ff5e6d71
Alexander Haumer <alexander.haumer@me.com>
2018-06-15 19:54:24 +0100

    back ground job not working, commented out

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,8 @@ class PagesController < ApplicationController
   def home
     @groups = Group.all
     @teams = Team.all
-    group_ids = [793, 794, 795, 796, 797, 798, 799, 800]
-    group_ids.each { |e| ResultsJob.perform_later(e) }
+    # group_ids = [793, 794, 795, 796, 797, 798, 799, 800]
+    # group_ids.each { |e| ResultsJob.perform_later(e) }
   end
 
   def faq
