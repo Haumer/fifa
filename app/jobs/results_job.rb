efb81f3d33e02c48d8bba7c4289cae699a767e00
Alexander Haumer <alexander.haumer@me.com>
2018-06-16 12:00:26 +0100

    redis

--- app/jobs/results_job.rb
+++ app/jobs/results_job.rb
@@ -1,40 +1,31 @@
 require "open-uri"
 require "json"
 
-class ResultsJob < ApplicationJob
+class UpdateLiveScoreJob < ApplicationJob
   queue_as :default
 
-  def perform(group_id)
-    root = "http://livescore-api.com/api-client"
-    groupstage = "#{ENV['RESULTS_URL']}"
-    response = open(root + groupstage + group_id.to_s)
-    response_body = response.read
-    json = JSON.parse(response_body)
+  def perform
+    group_ids = [793, 794, 795, 796, 797, 798, 799, 800]
+    group_ids.each do |group_id|
+      root = "http://livescore-api.com/api-client"
+      groupstage = "#{ENV['RESULTS_URL']}"
+      response = open(root + groupstage + group_id.to_s)
+      response_body = response.read
+      json = JSON.parse(response_body)
 
-    teams = Team.all
-    teams_group = teams.where(group_id: group_id)
-    puts "1"
-    teams_group.each { |e| p e.name }
-    puts "2"
-    json["data"]["table"].each do |data|
-      if data["name"] == "Morocco"
-        placeholder = "Morroco"
-        p placeholder
-      else
-        placeholder = data["name"]
-        p placeholder
+      teams = Team.all
+      teams_group = teams.where(group_id: group_id)
+
+      json["data"]["table"].each do |data|
+        team = teams_group.where(name: data["name"])
+        team.first.wins = data["won"]
+        team.first.losses = data["lost"]
+        team.first.draws = data["drawn"]
+        team.first.points = data["points"]
+        team.first.goals_for = data["goals_scored"]
+        team.first.goals_against = data["goals_conceded"]
+        team.first.save
       end
-      p "#{placeholder} is here"
-
-      team = teams_group.where(name: placeholder)
-      p team.first
-      team.first.wins = data["won"]
-      team.first.losses = data["lost"]
-      team.first.draws = data["drawn"]
-      team.first.points = data["points"]
-      team.first.goals_for = data["goals_scored"]
-      team.first.goals_against = data["goals_conceded"]
-      team.first.save
     end
   end
 end
