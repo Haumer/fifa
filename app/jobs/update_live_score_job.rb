1b39b584ab7b045596e9baa78da97164e6b3d930
Alexander Haumer <alexander.haumer@me.com>
2018-06-16 12:10:41 +0100

    trying second job

--- app/jobs/update_live_score_job.rb
+++ app/jobs/update_live_score_job.rb
@@ -4,28 +4,25 @@ require "json"
 class UpdateLiveScoreJob < ApplicationJob
   queue_as :default
 
-  def perform
-    group_ids = [793, 794, 795, 796, 797, 798, 799, 800]
-    group_ids.each do |group_id|
-      root = "http://livescore-api.com/api-client"
-      groupstage = "#{ENV['RESULTS_URL']}"
-      response = open(root + groupstage + group_id.to_s)
-      response_body = response.read
-      json = JSON.parse(response_body)
+  def perform(*group_id)
+    root = "http://livescore-api.com/api-client"
+    groupstage = "#{ENV['RESULTS_URL']}"
+    response = open(root + groupstage + group_id.to_s)
+    response_body = response.read
+    json = JSON.parse(response_body)
 
-      teams = Team.all
-      teams_group = teams.where(group_id: group_id)
+    teams = Team.all
+    teams_group = teams.where(group_id: group_id)
 
-      json["data"]["table"].each do |data|
-        team = teams_group.where(name: data["name"])
-        team.first.wins = data["won"]
-        team.first.losses = data["lost"]
-        team.first.draws = data["drawn"]
-        team.first.points = data["points"]
-        team.first.goals_for = data["goals_scored"]
-        team.first.goals_against = data["goals_conceded"]
-        team.first.save
-      end
+    json["data"]["table"].each do |data|
+      team = teams_group.where(name: data["name"])
+      team.first.wins = data["won"]
+      team.first.losses = data["lost"]
+      team.first.draws = data["drawn"]
+      team.first.points = data["points"]
+      team.first.goals_for = data["goals_scored"]
+      team.first.goals_against = data["goals_conceded"]
+      team.first.save
     end
   end
 end
