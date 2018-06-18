f3eb4cf12d071689c2109b9e697fa58998991d66
Alexander Haumer <alexander.haumer@me.com>
2018-06-18 22:28:02 +0100

    all matches data available with update_all_matches_job

--- app/jobs/update_all_matches_job.rb
+++ app/jobs/update_all_matches_job.rb
@@ -1,22 +1,70 @@
+require 'open-uri'
+
 class UpdateAllMatchesJob < ApplicationJob
   queue_as :default
 
   def perform
-    url = "http://worldcup.sfg.io/matches/today"
+    url = "http://worldcup.sfg.io/matches"
     response = open(url)
     json = JSON.parse(response.read)
 
     matches = Match.all
 
+
     json.each do |element|
       if element["status"] != "future"
+        p element
+        if element["home_team"]["country"] == "Morocco"
+          placeholder_home = "Morroco"
+          puts "placeholder_home is #{placeholder_home}"
+        elsif element["home_team"]["country"] == "Korea Republic"
+          placeholder_home = "South Korea"
+          puts "placeholder_home is #{placeholder_home}"
+        else
+          placeholder_home = element["home_team"]["country"]
+          puts "placeholder_home is #{placeholder_home}"
+        end
 
+        if element["away_team"]["country"] == "Morocco"
+          placeholder_away = "Morroco"
+          puts "placeholder_away is #{placeholder_away}"
+        elsif element["away_team"]["country"] == "Korea Republic"
+          placeholder_away = "South Korea"
+          puts "placeholder_away is #{placeholder_away}"
+        else
+          placeholder_away = element["away_team"]["country"]
+          puts "placeholder_away is #{placeholder_away}"
+        end
 
+        match_unit = matches.where(home_name: placeholder_home).where(away_name: placeholder_away)
 
+        match_unit.first.home_team_goals = element["home_team"]["goals"]
+        element["home_team_events"].each do |yellow_event|
+          if yellow_event["type_of_event"] == "yellow-card"
+            match_unit.first.home_team_yellow_cards += 1
+          end
+        end
 
+        element["home_team_events"].each do |red_event|
+          if red_event["type_of_event"] == "red-card"
+            match_unit.first.home_tean_red_cards += 1
+          end
+        end
 
+        match_unit.first.away_team_goals = element["away_team"]["goals"]
+        element["away_team_events"].each do |yellow_event|
+          if yellow_event["type_of_event"] == "yellow-card"
+            match_unit.first.away_team_yellow_cards += 1
+          end
+        end
 
+        element["away_team_events"].each do |red_event|
+          if red_event["type_of_event"] == "red-card"
+            match_unit.first.away_tean_red_cards += 1
+          end
+        end
 
+        match_unit.first.save
       end
     end
   end
